20d2aa3640227da2e120a264114dd47df7732d2a
kgracekennedy <kgracekennedy@gmail.com>
2014-11-16 17:46:41 -0500

    Labeling/commenting visualization

--- plothospitals.R
+++ plothospitals.R
@@ -1,6 +1,10 @@
-#Just a quick peek at the data
+#Just a quick peek at the heart attack data (30 day death rate)
+#suggested in part 1 of the instructions
 
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 outcome[, 11] <- as.numeric(outcome[, 11])
 HeartAttackData=outcome[,11]
-hist(HeartAttackData)
+hist(HeartAttackData,
+     main="30 Day Death Rate",
+     xlab="Mortality (As Percentage)"
+     )
